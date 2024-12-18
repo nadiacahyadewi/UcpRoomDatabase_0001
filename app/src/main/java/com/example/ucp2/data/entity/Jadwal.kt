58a2bc2a5147f50679e70ecabe810d8add10a0ba
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-18 17:40:42 +0700

    update Jadwal.kt: membuat foreign key

--- app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
@@ -1,9 +1,19 @@
 package com.example.ucp2.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "jadwal")
+@Entity(tableName = "jadwal",
+    foreignKeys = [
+        ForeignKey(
+            entity = Dokter::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("id"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
 data class Jadwal(
     @PrimaryKey
     val id: String,
