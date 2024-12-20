1fd777963eb2887071dab93207c6d5abccae9b1a
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-20 12:34:45 +0700

    membuat fungsi insert, View, detail, delete, dan update di JadwalDao

---
+++ app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
@@ -0,0 +1,26 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+interface JadwalDao {
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
+
+    @Query("SELECT * FROM jadwal ORDER BY id ASC")
+    fun getAllJadwal() : Flow<List<Jadwal>>
+
+    @Query("SELECT * FROM jadwal WHERE id = :id")
+    fun getJadwal(id: String) : Flow<Jadwal>
+
+    @Delete
+    suspend fun deleteJadwal(jadwal: Jadwal)
+
+    @Update
+    suspend fun updateJadwal(jadwal: Jadwal)
+
+}
